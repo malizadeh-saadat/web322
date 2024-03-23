9163c71e0ce2a164c4eef6927ece257bb4e828eb
Mahlegha <mahlega.alizadeh.2016@gmail.com>
2024-03-22 21:03:12 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,80 @@
+/********************************************************************************
+
+* WEB322 – Assignment 04
+
+*
+
+* I declare that this assignment is my own work in accordance with Seneca's
+
+* Academic Integrity Policy:
+
+*
+
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+
+*
+
+* Name: _______Mahlegha Alizadeh_____ Student ID: _157192220_____________ Date: ___2024/03/22___________
+
+*
+
+* Published URL: ___________________________________________________________
+
+*
+
+********************************************************************************/
+
+
+const legoData = require("./modules/legoSets");
+const path = require("path");
+
+const express = require('express');
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render("home")
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get("/lego/sets", async (req,res)=>{
+
+  let sets = [];
+
+  try{    
+    if(req.query.theme){
+      sets = await legoData.getSetsByTheme(req.query.theme);
+    }else{
+      sets = await legoData.getAllSets();
+    }
+
+    res.render("sets", {sets})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+  
+});
+
+app.get("/lego/sets/:num", async (req,res)=>{
+  try{
+    let set = await legoData.getSetByNum(req.params.num);
+    res.render("set", {set})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+});
+
+legoData.initialize().then(()=>{
+  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+});
